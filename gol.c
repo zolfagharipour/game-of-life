042353225632f96959409930d075f072d019b003
zolfagharipour <132170807+zolfagharipour@users.noreply.github.com>
2023-05-26 16:48:41 +0200

    Add files via upload

--- gol.c
+++ gol.c
@@ -1,20 +1,25 @@
 #include "gol.h"
 int		**map_open(char *file_name, t_map *map_d);
+int	**map_fit(int **map, t_map map_d);
 
 int	main()
 {
 	t_map map_d;
 	int	i = 0;
 	int	**map;
+	//int	**map1;
 
-	map = map_open("map3.txt", &map_d);
+	map = map_open("map2.txt", &map_d);
+	map = map_fit(map, map_d);
+	map_d.width = 280;
+	map_d.height = 70;
 
-	while (i < 100)
+	while (i < 1000)
 	{
 		write (1, "\033[0;0H", 7);
 		map_print(map, map_d);
 		map = next_step(map, map_d);
-		usleep (400000);
+		usleep (200000);
 		i++;
 	}
 	i = 0;
--- map_print.c
+++ map_print.c
@@ -11,11 +11,11 @@ void	map_print(int **map, t_map map_d)
 		{
 			if (map[j][i] == 0)
 			{
-				write (1, "- ", 2);
+				write (1, "-", 1);
 			}
 			else
 			{
-				write (1, "# ", 2);
+				write (1, "#", 1);
 			}
 			i++;
 		}
@@ -30,4 +30,46 @@ void	map_print(int **map, t_map map_d)
 		i++;
 	}
 	write (1, "\n", 1);
+}
+
+
+int	**map_fit(int **map, t_map map_d)
+{
+	int	**fit_map;
+	int	i;
+	int	j;
+	
+	fit_map = (int **)malloc(sizeof(int *) * 70);
+	map_d.j = 0;
+	j = 0;
+	while (map_d.j < 70)
+	{
+		fit_map[map_d.j] = (int *)malloc(sizeof(int) * 280);
+		map_d.i = 0;
+		i = 0;
+		while (map_d.i < 280)
+		{
+			if (map_d.i >= 140 - map_d.width /2 && map_d.i <= 140 + map_d.width /2 && map_d.j >= 35 - map_d.height /2 && map_d.j <= 35 + map_d.height /2)
+			{
+				fit_map[map_d.j][map_d.i] = map[j][i];
+				i++;
+			}
+			else
+				fit_map[map_d.j][map_d.i] = 0;	
+			
+			map_d.i++;
+		}
+		if (map_d.j >= 35 - map_d.height /2)
+			j++;
+		map_d.j++;
+
+	}
+	j = 0;
+	while(j < map_d.height)
+	{
+		free (map [j]);
+		j++;
+	}
+	free(map);
+	return (fit_map);
 }
